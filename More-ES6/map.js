d8cd3be441bd215e562376be99bc71b255a1bd0a
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2026-02-05 23:59:08 +0600

    map

---
+++ More-ES6/map.js
@@ -0,0 +1,32 @@
+const numbers= [1, 2, 3, 4, 5]
+const double = []
+for(const num of numbers){
+    const result = num * 2
+    double.push(result)
+}
+// console.log(double)
+
+// const doubleIt = x => x * 2;
+// const doubleNumbers = numbers.map(doubleIt)
+// console.log(doubleNumbers)
+
+// const doubleNumbers2 = numbers.map( x => x * 2 )
+// console.log(doubleNumbers2)
+
+const fiveTimes = numbers.map(num => num * 5)
+const square = numbers.map(num => num * num)
+// console.log(fiveTimes, square)
+
+const friday = ["egg", "paratha", "chicken", "beef", "fish"]
+const fridayItems = friday.map(item => item.toUpperCase())
+const fridayItemLength = friday.map(item => item.length)
+const firstLetter = friday.map(item => item[0])
+// console.log(fridayItems)
+// console.log(fridayItemLength)
+// console.log(firstLetter)
+
+const result = numbers.map((num, index) => {
+    console.log(num * index);
+    return index
+})
+console.log(result)
