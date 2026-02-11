2fd89aa2e74237a23c8fcd0d7c3c6df4b758a485
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2026-02-11 14:28:29 +0600

    check data type json to object

---
+++ Api-Concept/script.js
@@ -0,0 +1,19 @@
+const person = {
+    name: "John Doe",
+    age: 30,
+    email: "john.doe@example.com",
+    country: "USA",
+    occupation: "Software Engineer"
+}
+
+console.log(person, typeof person);
+
+// js object with notation
+
+// JSON.stringify() method converts object to JSON string
+const personJSON = JSON.stringify(person);
+console.log(personJSON, typeof personJSON);
+
+// JSON.parse() method parses JSON string to object
+const personObj = JSON.parse(personJSON);
+console.log(personObj, typeof personObj);
