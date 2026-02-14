596c90198bb98732c0ce5376f79b1347b31c0840
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2026-02-14 14:50:16 +0600

    fetch data show in display

--- Api-Concept/fetch.js
+++ Api-Concept/fetch.js
@@ -25,6 +25,15 @@ const loadTodos = () => {
     })
 }
 
+// users
+const loadUsers = () => {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        displayUsers(json);
+    })
+}
 
 // array of object
 const displayData = (posts) => {
@@ -64,6 +73,49 @@ const displayTodos = (todos) => {
         // 3.add div inside the container
         todoContainer.append(todoCard);
     });
+}
+
+
+//  {
+//     "id": 1,
+//     "name": "Leanne Graham",
+//     "username": "Bret",
+//     "email": "Sincere@april.biz",
+//     "address": {
+//       "street": "Kulas Light",
+//       "suite": "Apt. 556",
+//       "city": "Gwenborough",
+//       "zipcode": "92998-3874",
+//       "geo": {
+//         "lat": "-37.3159",
+//         "lng": "81.1496"
+//       }
+//     },
+//     "phone": "1-770-736-8031 x56442",
+//     "website": "hildegard.org",
+//     "company": {
+//       "name": "Romaguera-Crona",
+//       "catchPhrase": "Multi-layered client-server neural-net",
+//       "bs": "harness real-time e-markets"
+//     }
+//   },
+const displayUsers = (users) => {
+    const userContainer = document.getElementById("users-container");
+    userContainer.innerHTML = "";
+    users.forEach((user, i) => {
+        console.log("user data",user.address.city);
+        const userInfo = document.createElement("div");
+        userInfo.innerHTML =`<div class="user-info">
+        <p>${i+ 1}</p>
+        <h2>${user.name}</h2>
+        <p>${user.email}</p>
+        <p>${user.address.city}</p>
+        <p>${user.address.geo.lng}</p>
+        <p>${user.company.name}</p>
+        <p>${user.company.catchPhrase}</p>
+        </div>`
+        userContainer.append(userInfo)
+    })
 }
 
 // show default post
