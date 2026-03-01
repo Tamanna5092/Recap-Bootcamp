4239861fd81d393d5399f63d3e8842a2e2f09473
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2026-03-01 14:22:51 +0600

    bottle data load using useEffect

--- React-Intro/explore-core-react/core-concept/src/components/BottelCard/Bottles/Bottles.jsx
+++ React-Intro/explore-core-react/core-concept/src/components/BottelCard/Bottles/Bottles.jsx
@@ -1,23 +1,42 @@
-import { use } from "react";
+import { use, useEffect } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css";
 import { useState } from "react";
-import { addItemToCartLocalStorage } from "../../utilities/localStorage";
+import {
+  addItemToCartLocalStorage,
+  getCartFromLocalStorage,
+} from "../../utilities/localStorage";
 
 export default function Bottles({ bottlesPromise }) {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
   const bottles = use(bottlesPromise);
+
+  useEffect(() => {
+    const storagedCartIds = getCartFromLocalStorage();
+
+    const storedCart = [];
+    for (const id of storagedCartIds) {
+      console.log(id);
+      const bottleCart = bottles.find((bottle) => bottle.id === id);
+      if (bottleCart) {
+        storedCart.push(bottleCart);
+      }
+    }
+    console.log(storedCart);
+    setCart(storedCart);
+  }, [bottles]);
 
   const handleAddToCart = (bottle) => {
     console.log("bottle added", bottle);
-    const newCart = [...cart, bottle]
-    setCart(newCart)
-    addItemToCartLocalStorage(bottle.id)
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+    addItemToCartLocalStorage(bottle.id);
   };
 
   return (
     <div>
       <h1>Buy Awsome Watter Bottle: {bottles.length}</h1>
+      <h3>Cart: {cart.length}</h3>
       <div className="bottles-container">
         {bottles.map((bottle) => (
           <Bottle
